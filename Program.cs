2d143cfe9036533807d06c7fe4eef6597a526687
Gabi Thaler <gmth054@gmail.com>
2025-05-22 16:35:33 +0300

    aa

--- Program.cs
+++ Program.cs
@@ -10,11 +10,7 @@ namespace Idf
         static void Main(string[] args)
         {
 
-            List<string> s1 = new List<string> { "EFB", "ervbt", "Erg" };
-            Attacker a1 = new Attacker("RGV", 43, s1);
-            Uman u1 = new Uman("br", 45);
-            FighterAircraft f16 = new FighterAircraft("RGV", 43, s1, 34, 325, u1);
-            Console.WriteLine(f16.GetName());
+           
 
 
             Terrorist t1 = new Terrorist(1, ["M16"], "live", "han yons", "mhmod", 50);
@@ -23,29 +19,10 @@ namespace Idf
             Terrorist t4 = new Terrorist(4, ["knife"], "ded", "han yons", "yosof", 40);
             Terrorist t5 = new Terrorist(5, ["gun"], "live", "gbalia", "mhmod", 25);
             List<Terrorist> Terroristim = new List<Terrorist> { t1, t2, t3, t4, t5 };
-            //Hamas mjmm = new Hamas(1988, "sinoer", Terroristim);
-            //AMN amn = new AMN(mjmm.GetTeroristList());
-            //Console.WriteLine( amn.RiskCalculation().GetStatus());
+            List<string> atteacres = new List<string> { "f16", "Dron", "nisels" };
+            IdfOrganization IDF = new IdfOrganization(26051948, "eyalzamir", atteacres);
+            FighterAircraft f16 = new FighterAircraft("f16", 30000, new List<string> { "buildings" }, 300000, 8, new Uman("gabi", 28));
 
-            //Console.WriteLine("gbfh");
-            //Uman u1 = new Uman("dfgf", 5);
-            //Terrorist t1 = new Terrorist(5, ["dfs", "df"], "live","han yons", "dfs", 20);
-            //Console.WriteLine(t1.GetRank());
-            //IdfOrganization i1 = new IdfOrganization(1948, "aiil zamir", ["Air force", "Artillery", "Drones"]);
-            //i1.GetComendurName();
-            //foreach (string ability in i1.GetAttackCapabilities())
-            //{
-            //    Console.WriteLine($"{ability}");
-            //}
-            //foreach (string weapon in t1.GetWeapon())
-            //{
-            //    Console.WriteLine($"{weapon}");
-            //}
-            List<string> jbb = new List<string> { "Svfs", "swgver", "wdgs" };
-            Attacker aq1 = new Attacker("dwq", 44, jbb);
-            Console.WriteLine(aq1.GetName());
-            Console.WriteLine(aq1.GetTypeOfTarrget());
-            Console.WriteLine(aq1.GetAmountOfAttecors());
 
 
 
